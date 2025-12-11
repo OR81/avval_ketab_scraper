fdeeebcb80075ad87d65accbd1ab0196810da14b
omid <OmidRajabi81@gmail.com>
2025-12-11 09:24:15 +0330

    fix headless

--- avval_scraping.py
+++ avval_scraping.py
@@ -42,9 +42,9 @@ def save_to_database(data):
 
 # ----------------- Browser setup -----------------
 chrome_options = Options()
-# chrome_options.add_argument("--headless=new")
-# chrome_options.add_argument("--no-sandbox")
-# chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--headless=new")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
 # chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--window-size=1280,1024")
 
